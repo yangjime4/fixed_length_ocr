595fdbd02b9c9222e05206c28160d17c8dd4cda1
yangjin1 <yangjin1@aliyun.com>
2020-04-20 22:32:39 +0800

    add cnn model

---
+++ src/model.py
@@ -0,0 +1,27 @@
+from keras import Input, Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+
+IMAGE_HEIGHT = 32 - 8
+IMAGE_WIDTH = 94 - 8
+# 验证码长度
+CAPTCHA_LEN = 4
+# 验证码种类个数（0123abc...etc）
+CAPTCHA_TYPE_NUM = 33
+
+
+def build_model():
+    input_tensor = Input((IMAGE_HEIGHT, IMAGE_WIDTH, 1))
+    x = input_tensor
+    x = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 1))(x)
+    x = Conv2D(64, (3, 3), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Dropout(0.2)(x)
+    x = Conv2D(64, (3, 3), activation='relu')(x)
+    x = Conv2D(64, (3, 3), activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = Flatten()(x)
+    x = Dense(81, activation='relu')(x)
+    x = Dropout(0.2)(x)
+    x = [Dense(CAPTCHA_TYPE_NUM, activation='softmax', name='c%d' % (i + 1))(x) for i in range(CAPTCHA_LEN)]
+    model_cnn = Model(inputs=input_tensor, outputs=x)
+    return model_cnn
